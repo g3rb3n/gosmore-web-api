c9b21a53520cdf7071843d13b02cb475f1117ef6
Gerben <gerbenvaneerten@gmail.com>
2017-09-06 14:21:02 +0200

    Cleaned up utils

--- src/python/gosmorewebapi/util/json_response.py
+++ src/python/gosmorewebapi/util/json_response.py
@@ -4,14 +4,14 @@ from functools import wraps
 import datetime
 
 date_handler = lambda obj: (
-	obj.isoformat() + 'Z'
-	if isinstance(obj, datetime.datetime)
-	or isinstance(obj, datetime.date)
-	else None
+    obj.isoformat() + 'Z'
+    if isinstance(obj, datetime.datetime)
+    or isinstance(obj, datetime.date)
+    else None
 )
 
 def json_response(f):
-	@wraps(f)
-	def wrapped_function(*args, **kwargs):
-		return Response(json.dumps(f(*args, **kwargs), default=date_handler), mimetype='application/json')
-	return wrapped_function
+    @wraps(f)
+    def wrapped_function(*args, **kwargs):
+        return Response(json.dumps(f(*args, **kwargs), default=date_handler), mimetype='application/json')
+    return wrapped_function

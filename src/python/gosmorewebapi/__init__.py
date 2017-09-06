d3dbaac87ab9366cabd1b0efb5ae1e20570f3212
Gerben <gerbenvaneerten@gmail.com>
2017-09-06 14:22:30 +0200

    tabs to spaces

--- src/python/gosmorewebapi/__init__.py
+++ src/python/gosmorewebapi/__init__.py
@@ -2,8 +2,8 @@ import sys
 import logging
 
 logging.basicConfig( \
-	stream=sys.stderr, \
-	level=logging.WARN, \
-	format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s.%(funcName)s %(message)s', \
-	datefmt="%Y-%m-%dT%H:%M:%S" \
+    stream=sys.stderr, \
+    level=logging.WARN, \
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s.%(funcName)s %(message)s', \
+    datefmt="%Y-%m-%dT%H:%M:%S" \
 )
